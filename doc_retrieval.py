66ca7b3240bb71e2956ee9dd17b45f4c791c4374
Sam <108754148+smaley02@users.noreply.github.com>
2024-03-05 14:53:12 -0500

    Update doc_retrieval.py
    caught error where documents not loaded from folder

--- doc_retrieval.py
+++ doc_retrieval.py
@@ -47,68 +47,71 @@ for subdir, dirs, files in os.walk(reldir):
         # load documents
         print(f"\nloading documents from {curr_dir}...")
         documents = loader.load()
-
-        # split documents and store chunks
-        print("spliting documents...")
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
-        docs = text_splitter.split_documents(documents=documents)
-        docs = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        splits = text_splitter.split_documents(docs)
-        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(openai_api_key=openai_api_key))
-
-        # Retrieve and generate using the relevant snippets of the text
-        # simple retriever
-        retriever = vectorstore.as_retriever()
-        prompt = hub.pull("rlm/rag-prompt")
-        llm = ChatOpenAI(openai_api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0)
-
-        # multiquery retriever, appears to have better results
-        from langchain.retrievers.multi_query import MultiQueryRetriever
-        retriever = MultiQueryRetriever.from_llm(
-            retriever=vectorstore.as_retriever(), llm=llm
-        )
-        def format_docs(docs):
-            return "\n\n".join(doc.page_content for doc in docs)
-
-        # doing source retrieval
-        from langchain_core.runnables import RunnableParallel
-        rag_chain_from_docs = (
-            RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
-            | prompt
-            | llm
-            | StrOutputParser()
-        )
-        rag_chain_with_source = RunnableParallel(
-            {"context": retriever, "question": RunnablePassthrough()} # change retriever as necessary
-        ).assign(answer=rag_chain_from_docs)
-
-        # ask questions
-        print("querying documents...")
-        # print answer to a text document
-        filename = "output/" + curr_dir + ".txt"
-        file = open(filename, "w")
-        for question in questions:
-            result = rag_chain_with_source.invoke(question)
-
-            query = "\n############### QUESTION ###############\n" + question
-            answer = "\n############### RESPONSE ###############\n" + result["answer"]
-            sources = ""
-            for j in range(len(result["context"])):
-                sources += f"\n############### SOURCE #{j + 1} ###############\n"
-                sources += result["context"][j].metadata['source'] + "\n"
-                sources += result["context"][j].page_content + "\n"
-
-            file.write(query)
-            file.write(answer)
-            file.write(sources)
-            # print(answer)
-            # print(sources)
-
-        file.close()
-
-        # clear chroma database
-        # delete this line to query against ALL documents
-        vectorstore.delete_collection()
+        # catch error when no documents present in folder/error loading documents
+        if documents == []:
+            print("ERROR: loading documents from folder resulted in empty list")
+        else:
+            # split documents and store chunks
+            print("spliting documents...")
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+            docs = text_splitter.split_documents(documents=documents)
+            docs = loader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+            splits = text_splitter.split_documents(docs)
+            vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(openai_api_key=openai_api_key))
+    
+            # Retrieve and generate using the relevant snippets of the text
+            # simple retriever
+            retriever = vectorstore.as_retriever()
+            prompt = hub.pull("rlm/rag-prompt")
+            llm = ChatOpenAI(openai_api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0)
+    
+            # multiquery retriever, appears to have better results
+            from langchain.retrievers.multi_query import MultiQueryRetriever
+            retriever = MultiQueryRetriever.from_llm(
+                retriever=vectorstore.as_retriever(), llm=llm
+            )
+            def format_docs(docs):
+                return "\n\n".join(doc.page_content for doc in docs)
+    
+            # doing source retrieval
+            from langchain_core.runnables import RunnableParallel
+            rag_chain_from_docs = (
+                RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+                | prompt
+                | llm
+                | StrOutputParser()
+            )
+            rag_chain_with_source = RunnableParallel(
+                {"context": retriever, "question": RunnablePassthrough()} # change retriever as necessary
+            ).assign(answer=rag_chain_from_docs)
+    
+            # ask questions
+            print("querying documents...")
+            # print answer to a text document
+            filename = "output/" + curr_dir + ".txt"
+            file = open(filename, "w")
+            for question in questions:
+                result = rag_chain_with_source.invoke(question)
+    
+                query = "\n############### QUESTION ###############\n" + question
+                answer = "\n############### RESPONSE ###############\n" + result["answer"]
+                sources = ""
+                for j in range(len(result["context"])):
+                    sources += f"\n############### SOURCE #{j + 1} ###############\n"
+                    sources += result["context"][j].metadata['source'] + "\n"
+                    sources += result["context"][j].page_content + "\n"
+    
+                file.write(query)
+                file.write(answer)
+                file.write(sources)
+                # print(answer)
+                # print(sources)
+    
+            file.close()
+    
+            # clear chroma database
+            # delete this line to query against ALL documents
+            vectorstore.delete_collection()
 
     i += 1
